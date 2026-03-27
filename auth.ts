a091393a646e3bc0c00a2e21c3eec916d4f810ff
receptioniqbusiness <receptioniqbusiness@gmail.com>
2026-03-27 14:13:24 -0500

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -1,7 +1,12 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
   providers: [
     Google({
       clientId: process.env.AUTH_GOOGLE_ID!,
